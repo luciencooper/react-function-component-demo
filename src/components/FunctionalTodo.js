7d1dad678b4ebd47f4c1b337f2fa9ce70cb633f5
admin <admin@luk-5cg90579px.corp.capgemini.com>
2020-05-11 08:40:32 +0100

    More changes to functional todo

--- src/components/FunctionalTodo.js
+++ src/components/FunctionalTodo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FunctionalTodoList from './FunctionalTodoList';
 
 const FunctionalTodoApp = () => {
 
@@ -23,18 +24,18 @@ const FunctionalTodoApp = () => {
   return (
     <div>
       <h3>TODO</h3>
-      <TodoList items={this.state.items} />
-      <form onSubmit={this.handleSubmit}>
+      <FunctionalTodoList items={items} />
+      <form onSubmit={handleSubmit}>
         <label htmlFor="new-todo">
           What needs to be done?
         </label>
         <input
           id="new-todo"
-          onChange={this.handleChange}
-          value={this.state.text}
+          onChange={handleChange}
+          value={text}
         />
         <button>
-          Add #{this.state.items.length + 1}
+          Add #{items.length + 1}
         </button>
       </form>
     </div>
@@ -42,4 +43,4 @@ const FunctionalTodoApp = () => {
 
 }
 
-export default ClassTodoApp;
+export default FunctionalTodoApp;
