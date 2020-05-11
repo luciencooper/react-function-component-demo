a7bee97a464a274a231f890d3e96696647d14d13
admin <admin@luk-5cg90579px.corp.capgemini.com>
2020-05-11 08:23:24 +0100

    Class Todo List

---
+++ src/components/ClassTodoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ClassTodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default ClassTodoList;
